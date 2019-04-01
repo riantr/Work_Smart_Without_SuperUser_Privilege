50670fb8cbead861d674da06f6844e91a954736e
REN YongXiang <riantr@gmail.com>
2019-04-01 11:13:47 +0800

    Voila!

--- 0_debs_list.sh
+++ 0_debs_list.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 if [ -z $1 ];then
     echo 'apt search <Package> | grep "amd64 \| all"'
-    echo 'usage: 0_debs_list <packageName>'
+    echo 'usage: ./0_debs_list <packageName>'
 else 
-		apt-cache depends $1 > tmp 
+		apt-cache depends $1 | grep Depends > tmp 
 		if [ $? -eq 0 ];then
-		    echo $1 >> need_to_download
+		    echo $1 > need_to_download
 		fi
-        grep Depends: tmp | xargs -n2| sed 's/Depends://g' | xargs -n1 > need_to_download
+        grep Depends: tmp | xargs -n2| sed 's/Depends://g' | xargs -n1 >> need_to_download
+        rm tmp
         sed -i '/^$/d' need_to_download 
     for i in $(<need_to_download)
     do
@@ -19,6 +20,7 @@ else
 		fi
 	fi
     done
+    sed -i '/|/d' need_to_download
     declare -i number=0
     number=$(sed -n '$=' need_to_download)
     if [  $number -gt 1 ];then
