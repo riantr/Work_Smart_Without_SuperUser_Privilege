567a032f621f2affa7e3884f7a927c0877916ef0
REN YongXiang <riantr@gmail.com>
2019-04-01 15:49:48 +0800

    Voila!

--- 1_debs_extract.sh
+++ 1_debs_extract.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 if [ -f need_to_download ]; then
     echo --------------- $(date) ----------------- >> extracted_debs
+    [ ! -d backup ] && mkdir backup
     for i in $(<need_to_download);
 	do
 	    apt-get download $i
+        ls *.deb >> debs
 		dpkg -X $(ls *.deb) $HOME/opt/apt_deb
-        echo -e '\t[bin:\t\033[;44m' $(ls $HOME/opt/apt_deb/usr/bin |grep $i)'\033[0m]' | xargs echo $(ls *.deb |xargs -n1 |sed 's/deb/deb\t:\t[extracted]/g')|xargs echo -e $i'['$(date -d "20181101" +"%Y-%m-%d")']\t' >> extracted_debs
-		rm *.deb
+        echo -e '\t[bin:\t\033[;44m' $(ls $HOME/opt/apt_deb/usr/bin |grep $i)'\033[0m]' | xargs echo $(ls *.deb |xargs -n1 |sed 's/deb/deb[extracted]/g')|xargs echo -e $i'['$(date -d "20181101" +"%Y-%m-%d")']\t' >> extracted_debs
+		mv *.deb ./backup
 		echo -e '\033[;41m' $i' extracted to '$HOME'/opt/apt_deb.\033[0m\n'
 	done
 	rm need_to_download
