311ec1298408164c41ba85c7bed07f6edc05c6fc
REN YongXiang <riantr@gmail.com>
2019-04-02 14:06:51 +0000

    Voila!!

--- add_path_before_build.sh
+++ add_path_before_build.sh
@@ -10,6 +10,5 @@ read -n 1
 echo -e "export \033[;41m CPLUS_INCLUDE_PATH\033[0m=$DEB_EXTRACT_PATH/usr/include:$CPLUS_INCLUDE_PATH"
 read -n 1
 echo "------------------------------------------------------------------------------"
-echo -e "You should [\033[;41msource profile.apt_deb\033[0m] before build your program."
-echo -e "If you found it works, your should [\033[;41mcat profile.apt_deb >> ~/.profile && source ~/.profile\033[0m]. "
+echo -e "You should [\033[;41mcat bashrc.apt_deb>>~/.bashrc && source ~/.bashrc\033[0m] before build your program."
 echo "------------------------------------------------------------------------------"
