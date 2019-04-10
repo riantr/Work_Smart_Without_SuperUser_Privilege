6a2661430f8c1472fd5c0c4a336f0c811ae651ff
REN YongXiang <riantr@gmail.com>
2019-04-10 13:14:20 +0800

    Voila!

---
+++ check_protobuf_version.sh
@@ -0,0 +1,9 @@
+echo protobuf installed
+whereis protoc
+echo default protobuf
+which protoc
+echo check default protobuf version
+protoc --version
+echo system protobuf version
+/usr/local/bin/protoc --version
+
