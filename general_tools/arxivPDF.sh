ec26b508a4633e264bd90bc30202a374200b686d
riantr <riantr@gmail.com>
2019-07-05 01:23:17 +0800

    modified open_pdf

--- general_tools/arxivPDF.sh
+++ general_tools/arxivPDF.sh
@@ -3,4 +3,6 @@ echo -e "usage: arxivePDF xxxx.xxxxxx"
 elif [ ! -f $1.pdf ];then
 curl https://arxiv.org/pdf/$1.pdf > $1.pdf
 evince $1.pdf
+else
+evince $1.pdf
 fi
--- general_tools/open_pdf.sh
+++ general_tools/open_pdf.sh
@@ -1,1 +1,7 @@
+if [ -z $1 ]; then
+echo "usage: evince xxx.pdf"
+elif [ -f $1 ]; then
 evince $1
+else
+echo "Not found $1"
+fi
